80fafc1341c38636661fb5987dd7b82ee2242b74
Mrjaysa <joelaudu4@gmail.com>
2022-12-15 02:55:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container flex justify-content-center align-items-center">
+    <div class="card br-10">
+      <h1>Multiplication times table</h1>
+      
+      <form action="">
+        
+      </form>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 50%;
+  padding: 20px !important;
+}
+
+h1 {
+  text-decoration-line: underline;
+  text-underline-offset: 5px;
+}
+
+.container {
+  min-height: 100vh;
+}
+@media (max-width: 1024px) {
+}
+</style>
